9a4a04c3811f507bf4e635e1b595f4b42b60a4bb
iyanlucasd <iyanlucasd@gmail.com>
2020-11-10 13:14:12 -0300

    funcionando pre-heroku, node e handlebars no localhost

--- db.js
+++ db.js
@@ -1,19 +1,28 @@
-async function connect() {
-  if (global.connection && global.connection.state !== "disconnected")
-    return global.connection;
+async function connect(){
+  if(global.connection && global.connection.state !== 'disconnected')
+      return global.connection;
+
   const mysql = require("mysql2/promise");
-  const connection = await mysql.createConnection(
-    "mysql://root:Alucard931812@localhost:3306/crud"
-  );
-  console.log("conectou no mysql");
+  const connection = await mysql.createConnection("mysql://root:Alucard931812@localhost:3306/mydb");
+  console.log("Conectou no MySQL!");
   global.connection = connection;
   return connection;
 }
 
-async function selectTarefas(){
-    const conn = await connect();
-    const [rows] = await conn.query('SELECT * FROM lista;');
-    return await rows;
+async function selectCustomers(){
+  const conn = await connect();
+  const [rows] = await conn.query('SELECT * FROM Possui;');
+  return rows;
 }
 
-module.exports = {selectTarefas};
+async function selectUmaPeca(id){
+  const conn = await connect();
+  const sql = 'SELECT * FROM Possui WHERE FK_idPeca = ?;';
+  const values = id;
+  const [rows] = await conn.query(sql, values);
+  return rows;
+}
+
+
+// module.exports = {selectCustomers}
+module.exports = {selectUmaPeca}
